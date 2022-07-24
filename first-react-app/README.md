a635cad152c47d0b7ea61662a2b54b443bf72233
Malek Hamdan <malekhamdan82@gmail.com>
2022-07-24 18:48:24 +0300

    Create React App (Lab01)

---
+++ first-react-app/README.md
@@ -0,0 +1,5 @@
+# Getting Started with Create React App
+
+> > This Is My First Lab using **React,** hope you enjoy.
+
+### This project contain 3 images and describe them.
---
+++ first-react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from '../src/components/Header.js';
+import Main from '../src/components/Main.js';
+import Footer from '../src/components/Footer.js';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
